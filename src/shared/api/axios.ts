c2ea5a6cf27a405a167e0e09add57a786e3e97fa
gmtdn <failgim@gmail.com>
2026-05-25 23:02:44 +0300

    hot fix https api

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://app.tablecrm.com/api/v1',
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json'
     }
